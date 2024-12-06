5db1c56a2b6e9803ced9f53eb605e0beb7552985
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2024-12-06 21:56:23 +0500

    react with tailwind/vite

---
+++ 02-tailwind-and-props/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+ <>
+  <h1 className='bg-black text-left text-white p-20 m-20'>hello</h1>
+ </>
+  )
+}
+
+export default App
