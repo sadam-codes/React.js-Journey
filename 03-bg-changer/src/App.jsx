236437009ac216f16cd7002a31cafed169b3a225
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2024-12-08 00:14:07 +0500

    bg-changer

--- 03-bg-changer/src/App.jsx
+++ 03-bg-changer/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 
 function App() {
-  const [color, changeColor] = useState("black");
+  const [bgcolor, changeBgColor] = useState("black");
   return (
-    <div className="w-full h-screen duration-200" style={{ background: color }}>
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+    <div className="w-full h-screen" style={{ background: bgcolor }}>
+      <div className="flex flex-wrap justify-center inset-x-0 px-2 py-5">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg px-3 py-2 rounded-3xl bg-white">
+          {/* Red */}
           <button
-            className="px-3 outline-none bg-red-950 text-white rounded"
+            className="px-3 outline-none bg-red-950 text-white rounded-3xl"
             onClick={() => {
-              changeColor("red");
+              changeBgColor("red");
             }}
           >
             Red
@@ -18,21 +19,61 @@ function App() {
           {/* black */}
 
           <button
-            className="px-3 outline-none bg-black text-white rounded"
+            className="px-3 outline-none bg-black text-white rounded-3xl"
             onClick={() => {
-              changeColor("black");
+              changeBgColor("black");
             }}
           >
             Black
           </button>
+
           {/* blue */}
           <button
-            className="px-3 outline-none bg-blue-950 text-white rounded"
+            className="px-3 outline-none bg-blue-950 text-white rounded-3xl"
             onClick={() => {
-              changeColor("blue");
+              changeBgColor("blue");
             }}
           >
             Blue
+          </button>
+
+          {/* yellow */}
+          <button
+            className="px-3 outline-none bg-yellow-950 text-white rounded-3xl"
+            onClick={() => {
+              changeBgColor("yellow");
+            }}
+          >
+            Yellow
+          </button>
+
+          {/* purple */}
+          <button
+            className="px-3 outline-none bg-purple-950 text-white rounded-3xl"
+            onClick={() => {
+              changeBgColor("purple");
+            }}
+          >
+            Purple
+          </button>
+          {/* white */}
+
+          <button
+            className="px-3 outline-none bg-black text-slate-200 rounded-3xl"
+            onClick={() => {
+              changeBgColor("white");
+            }}
+          >
+            White
+          </button>
+          {/* green */}
+          <button
+            className="px-3 outline-none bg-green-950 text-white rounded-3xl"
+            onClick={() => {
+              changeBgColor("green");
+            }}
+          >
+            Green
           </button>
         </div>
       </div>
