493e4f6c554e7eb09ed2bb3ef17599f3f736c03f
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2024-12-25 21:49:58 +0500

    react-routing

--- 04-Router/src/App.jsx
+++ 04-Router/src/App.jsx
@@ -1,3 +1,30 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Github from "./components/Github";
+import User from "./components/User";
+import Contact from "./components/Contact";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Header />
+        {/* Define Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/github" element={<Github />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
