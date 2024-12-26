ddebc7bdb06b94bb1df63ff7d84468b3504c5173
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2024-12-26 22:03:45 +0500

    updated

--- 04-Router/src/App.jsx
+++ 04-Router/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Header from "./components/Header";
 import About from "./components/About";
 import Footer from "./components/Footer";
@@ -8,18 +13,19 @@ import Github from "./components/Github";
 import User from "./components/User";
 import Contact from "./components/Contact";
 
+
 const App = () => {
   return (
     <Router>
       <div>
         <Header />
-        {/* Define Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/github" element={<Github />} />
           <Route path="/user" element={<User />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="user/:userid" element={<User />} />
         </Routes>
         <Footer />
       </div>
