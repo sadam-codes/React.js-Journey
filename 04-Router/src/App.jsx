b41db1d661208d0308da2cfd50aefd4c0985470d
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2024-12-26 22:25:30 +0500

    lazy loading

--- 04-Router/src/App.jsx
+++ 04-Router/src/App.jsx
@@ -1,35 +1,29 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
-import Header from "./components/Header";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Github from "./components/Github";
-import User from "./components/User";
-import Contact from "./components/Contact";
+import React, { Suspense, lazy } from 'react';
+
+const Home = lazy(() => import("./components/Home"));
+const About = lazy(() => import("./components/About"));
+const Github = lazy(() => import("./components/Github"));
+const User = lazy(() => import("./components/User"));
+const Contact = lazy(() => import("./components/Contact"));
 
 const App = () => {
   return (
     <Router>
       <div>
         <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/github" element={<Github />} />
-          <Route exact path="/user" element={<User />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="user/:userid" element={<User />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/github" element={<Github />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="user/:userid" element={<User />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </div>
     </Router>
   );
 };
-
-export default App;
